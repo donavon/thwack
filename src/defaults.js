401cbb54f47b5c55536c661f414045de1903fafa
Ian Sutherland <ian@iansutherland.ca>
2020-04-16 11:19:44 -0600

    Only reference window when running in a browser environment

--- src/defaults.js
+++ src/defaults.js
@@ -1,19 +1,22 @@
 export const APPLICATION_JSON = 'application/json';
 export const CONTENT_TYPE = 'content-type';
 
-const {
-  fetch,
-  location: { origin, pathname },
-} = window;
-
 export const defaultOptions = {
-  fetch,
-  baseURL: `${origin}${pathname}`,
   params: {},
   headers: {
     accept: `${APPLICATION_JSON}, text/plain, */*`,
   },
 };
+
+if (typeof window !== 'undefined') {
+  const {
+    fetch,
+    location: { origin, pathname },
+  } = window;
+
+  defaultOptions.fetch = fetch;
+  defaultOptions.baseURL = `${origin}${pathname}`;
+}
 
 export const defaultParserMap = {
   [APPLICATION_JSON]: 'json',
