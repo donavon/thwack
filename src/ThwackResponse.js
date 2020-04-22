2031653c3c61cad86556768002a9c8ccf1c0d639
donavon <github@donavon.com>
2020-04-22 13:53:40 -0400

    fixed a bug w/ThwackResponse headers

--- src/ThwackResponse.js
+++ src/ThwackResponse.js
@@ -4,15 +4,16 @@ export default class ThwackResponse {
       status = 200,
       statusText = `Status ${status}`,
       data,
-      headers = [],
+      headers = {},
     } = response;
     this.status = status;
     this.statusText = statusText;
     this.ok = status >= 200 && status < 300;
     this.data = data;
-    this.headers = Array.isArray(headers)
-      ? headers
-      : Object.fromEntries(headers.entries());
+    this.headers =
+      typeof headers.entries === 'function' // TODO why can't I do `headers instanceof Headers`?
+        ? Object.fromEntries(headers.entries())
+        : headers;
     this.options = options;
     this.response = response;
   }
