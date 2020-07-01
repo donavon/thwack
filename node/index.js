075c30248f90f32d9043d7b7216049d49954d517
donavon <github@donavon.com>
2020-07-01 12:24:44 -0400

    unbreak node with typescript

--- node/index.js
+++ node/index.js
@@ -1,6 +1,5 @@
-// This is the file used when you import from 'thwack/node'
 const fetch = require('node-fetch');
-const thwack = require('.');
+const thwack = require('..');
 
 thwack.defaults.fetch = fetch;
 
--- node10/index.js
+++ node10/index.js
@@ -1,7 +1,6 @@
-// This is the file used when you import from 'thwack/node10'
 require('core-js/features/array/flat');
 require('core-js/features/object/from-entries');
 
-const thwack = require('./node');
+const thwack = require('../node');
 
 module.exports = thwack;
